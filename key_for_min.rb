a9b751bbcedee6542603aafbba91795056cdd65e
naomijoyce <naomijoyceb@gmail.com>
2018-10-23 20:05:47 -0400

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,16 @@
 
 def key_for_min_value(name_hash)
 
+  if name_hash.empty? == true
+    return nil
+  else
+    current = nil
+    hash_key = nil
+    name_hash.each do |key, value|
+      if current == nil || value < current
+      current = value
+      hash_key = key
+    end
+  end
+    return hash_key
 end
